23b379b7bcdc9b44edb96c53d1c3fd30d4f71ec7
intergalact <roshanaque@gmail.com>
2019-04-03 23:04:02 +0200

    GridBayLayout isnt working :(

--- KeyBarricade V03/src/Interface.java
+++ KeyBarricade V03/src/Interface.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.nio.file.Paths;
 
 public class Interface extends JFrame {
+
+    Map map;
 
     public Interface() throws HeadlessException {
         setupFrame();
@@ -12,13 +17,70 @@ public class Interface extends JFrame {
     public void setupFrame(){
         setTitle("Key Barricade");
         setSize(new Dimension(600, 700));
-        setLayout(new BorderLayout());
+        setLayout(new GridBagLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
 
     public void addComponents(){
-        add(new Map());
+        map = new Map();
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.gridwidth = 2;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.fill = GridBagConstraints.BOTH;
+        add(map, gc);
+
+        JPanel panelChooseLevel = new JPanel(new GridLayout(3,1));
+
+        //create buttons and fill panelChooseLevel
+        JRadioButton buttonLevel1 = new JRadioButton("Level 1");
+        JRadioButton buttonLevel2 = new JRadioButton("Level 2");
+        JRadioButton buttonLevel3 = new JRadioButton("Level 3");
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(buttonLevel1);
+        panelChooseLevel.add(buttonLevel1);
+        buttonGroup.add(buttonLevel2);
+        panelChooseLevel.add(buttonLevel2);
+        buttonGroup.add(buttonLevel3);
+        panelChooseLevel.add(buttonLevel3);
+
+        buttonLevel1.addActionListener((ActionEvent e) -> {
+            map.fillMapLetters(Paths.get("level1-letters"));
+            map.repaint();
+        });
+
+        buttonLevel2.addActionListener((ActionEvent e) -> {
+            map.fillMapLetters(Paths.get("level2-letters"));
+            map.repaint();
+        });
+
+        buttonLevel3.addActionListener((ActionEvent e) -> {
+            map.fillMapLetters(Paths.get("level3-letters"));
+            map.repaint();
+        });
+
+        JPanel panelReset = new JPanel(new GridLayout());
+        JRadioButton buttonReset = new JRadioButton("Reset");
+        panelReset.add(buttonReset);
+
+        gc.weightx = 0;
+        gc.weighty = 0;
+        gc.gridx = 0;
+        gc.gridy = 1;
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.anchor = GridBagConstraints.LINE_START;
+        add(panelChooseLevel, gc);
+//        gc.weightx = GridBagConstraints.REMAINDER;
+//        gc.weighty = GridBagConstraints.REMAINDER;
+        gc.gridheight = 1;
+        gc.gridwidth = 1;
+        gc.gridx = 1;
+        gc.gridy = 1;
+//        gc.anchor = GridBagConstraints.LINE_START;
+        add(panelReset, gc);
     }
 
     public void reset() {
--- KeyBarricade V03/src/Map.java
+++ KeyBarricade V03/src/Map.java
@@ -1,9 +1,5 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,12 +7,10 @@ import java.nio.file.Paths;
 
 public class Map extends JPanel {
 
+    private static final int LEVELDIM = 10; //nr of rows and columns
     private static final int LENGTH = 200;
     private static final int WIDTH = 200;
-//    private Tile tile; //associatie
-//    private Interface anInterface;
     private Tile[][] mapLetters;
-    private static final int LEVELDIM = 10; //nr of rows and columns
 
     public Map() {
         mapLetters = new Tile[LEVELDIM][LEVELDIM];
@@ -50,8 +44,8 @@ and fill the map with the corresponding GamePiece.
                         case 'k':
                             mapLetters[row][column] = new Key(row,column,290);
                             break;
-                        case '2':case '3':
-                            mapLetters[row][column] = new Key(row,column, 100*(ch-'0'));
+                        case '2':
+                            mapLetters[row][column] = new Key(row,column, 100);
                             break;
                         case 'B':
                             mapLetters[row][column] = new Barricade(row,column,130);
@@ -77,31 +71,6 @@ and fill the map with the corresponding GamePiece.
             e.printStackTrace();
         }
     }
-
-//    public void addComponents(){
-//
-//        JPanel panelChooseLevel = new JPanel();
-//
-//        //create buttons and fill panelChooseLevel
-//        JRadioButton buttonLevel1 = new JRadioButton("Level 1");
-//        JRadioButton buttonLevel2 = new JRadioButton("Level 2");
-//        JRadioButton buttonLevel3 = new JRadioButton("Level 3");
-//        ButtonGroup buttonGroup = new ButtonGroup();
-//        buttonGroup.add(buttonLevel1);
-//        panelChooseLevel.add(buttonLevel1);
-//        buttonGroup.add(buttonLevel2);
-//        panelChooseLevel.add(buttonLevel2);
-//        buttonGroup.add(buttonLevel3);
-//        panelChooseLevel.add(buttonLevel3);
-//
-//        buttonLevel2.addActionListener((ActionEvent e) -> {
-////            map.fillMapLetters(Paths.get("level2-letters"));
-////            start(map.getMapLetters());
-////            panelGrid.revalidate();
-////            panelGrid.repaint();
-//            update(Paths.get("level2-letters"));
-//        });
-//    }
 
     public void start(){
         fillMapLetters(Paths.get("level1-letters"));
